626990d04ef09ca4fab21b3a2900ab0ea8dc2191
Dustin Breese <dbreese@salesforce.com>
2022-10-17 10:44:27 -0600

    Converted viewAccountRecord to use lightning-record-form
    Also showing an example on how to view and interact with drafts.

--- force-app/main/default/lwc/viewAccountRecord/__tests__/viewAccountRecord.test.js
+++ force-app/main/default/lwc/viewAccountRecord/__tests__/viewAccountRecord.test.js
@@ -16,10 +16,8 @@ describe('c-view-account-record', () => {
         const element = createElement('c-view-account-record', {
             is: ViewAccountRecord
         })
+        element.objectApiName = "objectApiName"
         document.body.appendChild(element)
-
-        // Initialize this to a value, as .toLowerCase() is called on it
-        element.objectApiName = "cardIconName"
 
         // Emit mock record into the wired field
         // eslint-disable-next-line @lwc/lwc/no-unexpected-wire-adapter-usages
@@ -27,13 +25,19 @@ describe('c-view-account-record', () => {
 
         // Resolve a promise to wait for a re-render of the new content
         return Promise.resolve().then(() => {
-            const nameField = element.shadowRoot.querySelector(
-                'lightning-layout-item[class="record-name"]'
-            )
+            const objectField = element.shadowRoot.querySelector('lightning-layout-item[data-id="objectApiNameId"]')
+            expect(objectField.textContent).toBe(element.objectApiName)
 
-            const nameFieldMock = mockGetRecord.fields.Name.value
-            
-            expect(nameField.textContent).toBe(nameFieldMock)
+            const nameField = element.shadowRoot.querySelector('lightning-layout-item[data-id="nameId"]')
+            expect(nameField.textContent).toBe(mockGetRecord.fields.Name.value)
+
+            const iconField = element.shadowRoot.querySelector('lightning-icon[data-id="iconId"]')
+            expect(iconField.iconName).toBe("standard:" + element.objectApiName.toLowerCase())
+
+            // our sample data also has 2 draft edits (name and phone)
+            const draftEdits = element.shadowRoot.querySelectorAll('li')
+            expect(draftEdits[0].textContent).toBe("Name=Old Name")
+            expect(draftEdits[1].textContent).toBe("Phone=+1-719-555-1212")
         })
     })
 })
