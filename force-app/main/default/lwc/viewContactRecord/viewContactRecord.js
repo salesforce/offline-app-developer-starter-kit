88ba4960b22fe7e812fc16dc65d818815b1b9ebb
Adam Liechty <aliechty@salesforce.com>
2023-03-17 14:35:29 -0700

    Contact view uses fields rather than layout, matching Account view

--- force-app/main/default/lwc/viewContactRecord/viewContactRecord.js
+++ force-app/main/default/lwc/viewContactRecord/viewContactRecord.js
@@ -1,14 +1,29 @@
 import { LightningElement, api, wire } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
 
-import NAME from "@salesforce/schema/Account.Name";
-const fields = [NAME];
+import NAME_FIELD from "@salesforce/schema/Contact.Name";
+import TITLE_FIELD from "@salesforce/schema/Contact.Title";
+import PHONE_FIELD from "@salesforce/schema/Contact.Phone";
+import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
+import MAILINGADDRESS_FIELD from "@salesforce/schema/Contact.MailingAddress";
+import DEPARTMENT_FIELD from "@salesforce/schema/Contact.Department";
+
+const FIELDS = [
+  NAME_FIELD,
+  TITLE_FIELD,
+  PHONE_FIELD,
+  EMAIL_FIELD,
+  MAILINGADDRESS_FIELD,
+  DEPARTMENT_FIELD,
+];
 
 export default class ViewContactRecord extends LightningElement {
   @api recordId;
   @api objectApiName;
 
-  @wire(getRecord, { recordId: "$recordId", fields })
+  fields = FIELDS;
+
+  @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
   record;
 
   get cardIconName() {
