12fced152f46a10c5fb092522daff77f5cb68e1e
Adam Liechty <aliechty@salesforce.com>
2023-06-08 11:34:37 -0700

    Sample graphql lookup of related records (#27)
    * Sample graphql lookup of related records
    
    This example focuses on a new accountRelatedContacts sample component.
    
    It `@wire`s `graphql` to run a primable query to fetch the top 5 related Contacts.
    
    For completeness, a simple viewContactRecord LWC is proveded, so that tapping a Contact in the list can navigate to view the Contact.
    
    * lint fix
    
    * Contact view uses fields rather than layout, matching Account view
    
    * eslint-plugin-lwc-graph-analyzer 0.6.1
    
    * Remove graphql metaschema directives.
    
    These will be supplied in 244+ automatically
    
    * Apply same fix from #29
    
    * coment out related contacts by default
    
    * Fix lint warnings
    
    * Update @salesforce/sfdx-lwc-jest for graphql mocks
    
    * + test accountRelatedContacts LWC

---
+++ force-app/main/default/lwc/accountRelatedContacts/accountRelatedContacts.js
@@ -0,0 +1,76 @@
+import { LightningElement, api, wire } from "lwc";
+import { NavigationMixin } from "lightning/navigation";
+
+import { graphql, gql } from "lightning/uiGraphQLApi";
+
+export default class AccountRelatedContacts extends NavigationMixin(
+  LightningElement
+) {
+  @api recordId;
+
+  // https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_graphql_relationships
+  @wire(graphql, {
+    query: gql`
+      query accountWithChildContacts($recordId: ID) {
+        uiapi {
+          query {
+            Account(where: { Id: { eq: $recordId } }) {
+              edges {
+                node {
+                  Contacts {
+                    edges {
+                      node {
+                        Id
+                        Name {
+                          value
+                        }
+                        Phone {
+                          value
+                        }
+                        Email {
+                          value
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: "$graphqlVariables",
+    operationName: "accountWithChildContacts",
+  })
+  graphqlResult({ data /* errors */ }) {
+    this.contacts = null;
+    if (data && data.uiapi.query.Account) {
+      const accounts = data.uiapi.query.Account.edges;
+      if (accounts && accounts[0]) {
+        this.contacts = accounts[0].node.Contacts.edges.map((e) => e.node);
+      }
+    }
+  }
+  contacts;
+
+  get graphqlVariables() {
+    return {
+      recordId: this.recordId,
+    };
+  }
+
+  contactClick(event) {
+    const { id } = event.currentTarget.dataset;
+    this[NavigationMixin.Navigate]({
+      type: "standard__quickAction",
+      attributes: {
+        actionName: "Contact.view",
+      },
+      state: {
+        recordId: id,
+        objectApiName: "Contact",
+      },
+    });
+  }
+}
