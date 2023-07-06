0e765675beb977d8d4c07bfcf303ca35e6d2495e
Alex Sikora <asikora@salesforce.com>
2023-07-06 12:52:59 -0500

    Remove unnecessary wire import

--- force-app/main/default/lwc/fileUpload/fileUpload.js
+++ force-app/main/default/lwc/fileUpload/fileUpload.js
@@ -1,4 +1,4 @@
-import { LightningElement, api, track, wire } from "lwc";
+import { LightningElement, api, track } from "lwc";
 import {
   unstable_createContentDocumentAndVersion,
   createRecord,
