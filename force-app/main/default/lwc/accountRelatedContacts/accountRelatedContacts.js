a2c0906fdfd5040c24ae871de9c93feef4b95d77
Mahendran Shanmugavelu <mshanmugavelu@salesforce.com>
2023-08-22 22:32:36 +0000

    run prettier

--- force-app/main/default/lwc/accountRelatedContacts/accountRelatedContacts.js
+++ force-app/main/default/lwc/accountRelatedContacts/accountRelatedContacts.js
@@ -10,9 +10,8 @@ export default class AccountRelatedContacts extends NavigationMixin(
   @api recordId;
 
   get accountQuery() {
-
     if (!this.recordId) return undefined;
-  
+
     return gql`
       query accountWithChildContacts($recordId: ID) {
         uiapi {
@@ -43,7 +42,7 @@ export default class AccountRelatedContacts extends NavigationMixin(
         }
       }
     `;
-    }
+  }
 
   // https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_graphql_relationships
   //
